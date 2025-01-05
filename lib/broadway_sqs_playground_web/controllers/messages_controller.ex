9775b618d95fa0d9bae2ffc883db9094c66fc3dd
Guilhermo Masid <guilhermomasid@gmail.com>
2025-01-05 11:05:25 -0300

    send messages working

--- lib/broadway_sqs_playground_web/controllers/messages_controller.ex
+++ lib/broadway_sqs_playground_web/controllers/messages_controller.ex
@@ -2,6 +2,21 @@ defmodule BroadwaySqsPlaygroundWeb.MessagesController do
   use BroadwaySqsPlaygroundWeb, :controller
 
   def send_message(conn, _params) do
-    conn |> json(%{ok: true})
+    config = [
+      scheme: "http://",
+      host: "localhost",
+      port: 4566,
+      access_key_id: "",
+      secret_access_key: ""
+    ]
+
+    queue_url = "http://localhost:4566/000000000000/sqs-demo"
+    message = Poison.encode!(%{"foo" => "bar"})
+
+    queue_url
+    |> ExAws.SQS.send_message(message)
+    |> ExAws.request!(config)
+
+    conn |> json(%{success: true})
   end
 end
